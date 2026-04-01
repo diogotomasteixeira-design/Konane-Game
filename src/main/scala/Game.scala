4443463ecbef470e0176a973109ffa8b5f728e26
Diogo <diogo.tomas.teixeira@gmail.com>
2026-04-01 17:08:24 +0100

    T1 e initBoard

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,8 +1,27 @@
-import Types.Coord2D
+import Types.{Board, Coord2D, Stone}
+
+import scala.collection.parallel.immutable.ParMap
 
 object Game {
+
+  def initBoard(rows: Int, cols: Int): Board ={
+    val coords = for
+      r <- 0 until rows
+      c <- 0 until cols
+    yield (r, c)
+  
+    coords.foldLeft(ParMap.empty[Coord2D, Stone]) { (acc, coord) =>
+      val (r, c) = coord
+      val stone = if (r + c) % 2 == 0 then Stone.Black else Stone.White
+      acc + (coord -> stone)
+    }
+
+  }
+
   def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom): (Coord2D, MyRandom) = {
-    
+    val (n, nextRandom) = rand.nextInt
+    val coord = math.abs(n) % lstOpenCoords.length
+    (lstOpenCoords(coord), nextRandom)
   }
 
 }
