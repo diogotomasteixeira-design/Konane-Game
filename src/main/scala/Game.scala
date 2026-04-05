8faf87b277162883b3f330167564bf59917acd3d
Diogo <diogo.tomas.teixeira@gmail.com>
2026-04-05 16:49:38 +0100

    Parte T2

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -4,18 +4,24 @@ import scala.collection.parallel.immutable.ParMap
 
 object Game {
 
-  def initBoard(rows: Int, cols: Int): Board ={
+  def initBoard(rows: Int, cols: Int): (Board, List[Coord2D]) = {
     val coords = for
       r <- 0 until rows
       c <- 0 until cols
     yield (r, c)
-  
-    coords.foldLeft(ParMap.empty[Coord2D, Stone]) { (acc, coord) =>
+
+    val fullBoard = coords.foldLeft(ParMap.empty[Coord2D, Stone]) { (acc, coord) =>
       val (r, c) = coord
       val stone = if (r + c) % 2 == 0 then Stone.Black else Stone.White
       acc + (coord -> stone)
     }
 
+    val centerBlack = (rows / 2, cols / 2)
+    val centerWhite = (rows / 2, cols / 2 - 1)
+    val board = fullBoard - centerBlack - centerWhite
+
+    val openCoords = List(centerBlack, centerWhite)
+    (board, openCoords)
   }
 
   def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom): (Coord2D, MyRandom) = {
@@ -24,4 +30,31 @@ object Game {
     (lstOpenCoords(coord), nextRandom)
   }
 
+  def play(board: Board, player: Stone, coordFrom: Coord2D, coordTo: Coord2D, lstOpenCoords: List[Coord2D]): (Option[Board], List[Coord2D]) = {
+    if (validMove(board, player, coordFrom, coordTo, lstOpenCoords)) {
+      val captured = ((coordFrom._1 + coordTo._1) / 2, (coordFrom._2 + coordTo._2) / 2)
+      val newBoard = board - coordFrom - captured + (coordTo -> player)
+      val newOpenCoords = lstOpenCoords.filterNot(_ == coordTo) :+ coordFrom :+ captured
+      (Some(newBoard), newOpenCoords)
+    } else {
+      (None, lstOpenCoords)
+    }
+
+  }
+
+  def validMove(board: Board, player: Stone, coordFrom: Coord2D, coordTo: Coord2D, lstOpenCoords: List[Coord2D]): Boolean = {
+    val captured = ((coordFrom._1 + coordTo._1) / 2, (coordFrom._2 + coordTo._2) / 2)
+    val cap: Option[Stone] = board.get(captured)
+
+    val capturedIsEnemy = cap match {
+      case Some(Stone.Black) =>
+        player == Stone.White
+      case Some(Stone.White) =>
+        player == Stone.Black
+      case None =>
+        false
+    }
+    val res: Boolean = capturedIsEnemy && board.contains(coordTo)
+    res
+  }
 }
